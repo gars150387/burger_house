c3b1274db48aa3f5481d0234454c0dcaecaa3136
Gustavo <gar.santeliz@gmail.com>
2021-12-14 18:54:43 -0500

    set up style for images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -15,40 +15,24 @@ export const ControlledCarousel = () =>{
               <Carousel activeIndex={index} onSelect={handleSelect}>
                 <Carousel.Item>
                   <img
-                    className="d-block w-100"
+                    className="img-carousel"
                     src="https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Smash-Burgers_EXPS_TOHcom20_246232_B10_06_10b.jpg"
                     alt="First slide"
                   />
-                  <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
-                    className="d-block w-100"
+                    className="img-carousel"
                     src="https://www.recipetineats.com/wp-content/uploads/2019/08/Avocado-Chicken-Burgers_9.jpg"
                     alt="Second slide"
                   />
-          
-                  <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
-                    className="d-block w-100"
+                    className="img-carousel"
                     src="https://images.ricardocuisine.com/services/recipes/bruger.png"
                     alt="Third slide"
                   />
-          
-                  <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                  </Carousel.Caption>
                 </Carousel.Item>
               </Carousel>
             );
