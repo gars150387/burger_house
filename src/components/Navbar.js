dbe695ed795fee395c112ad96acbd5a6ffbbfa8c
Gustavo <gar.santeliz@gmail.com>
2021-12-14 19:47:53 -0500

    rename options

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,9 +19,9 @@ export const NavbarMain = ()=>{
       </Offcanvas.Header>
       <Offcanvas.Body>
         <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+          <Nav.Link href="#action1">Inicio</Nav.Link>
+          <Nav.Link href="#action2">Quienes Somos</Nav.Link>
+          <NavDropdown title="Menu" id="offcanvasNavbarDropdown">
             <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
             <NavDropdown.Divider />
