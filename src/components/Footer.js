c4b52ae5b9e48703a761fcbf64747ff081e64ad9
Gustavo <gar.santeliz@gmail.com>
2021-12-14 19:46:45 -0500

    trying to link icons with direct links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,10 @@ export const Footer = () => {
     return (
         <Card id="footer" className="bg-dark">
             <div className="">
-                <BsInstagram className="" style={{ color: "white", marginRight: "10px" }} /><FaFacebookF className="" style={{ color: "white", marginRight: "10px"  }} />
-                <FiTwitter style={{ color: "white", marginRight: "10px" }} /> <ImWhatsapp style={{ color: "white"}}/>
+                <BsInstagram onClick={"https://www.instagram.com/comidazipa/?hl=en"} className="" style={{ color: "white", marginRight: "10px" }} />
+                <FaFacebookF className="" style={{ color: "white", marginRight: "10px"  }} />
+                <FiTwitter style={{ color: "white", marginRight: "10px" }} /> 
+                <ImWhatsapp style={{ color: "white"}}/>
             </div>
             <Card.Header className="text-end" style={{ color: "white" }}>Copyrights by Burguer House</Card.Header>
         </Card>
